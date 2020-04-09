dbacd390a897be15adbb13e24c9bce299d578461
William Schwindt <williammschwindt@gmail.com>
2020-04-09 15:27:19 -0700

    all requests working

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./database.js');
 
 const server = express();
 server.use(express.json());
@@ -8,23 +9,72 @@ server.get('/', (req, res) => {
 });
 
 server.post('/api/users', (req, res) => {
-    //Creates a user using the information sent inside the request body
+    if(!req.body.name || !req.body.bio) {
+        return res.status(400).json({ message: "Need a user name and bio" })
+    };
+
+    let newUser = db.createUser({
+        name: req.body.name,
+        bio: req.body.bio
+    });
+
+    if(!newUser) {
+        return res.status(500).json({ message: "There was an error while saving the user to the database" });
+    } else {
+        return res.status(201).json(newUser);
+    }
 });
 
 server.get('/api/users', (req, res) => {
-    //Returns an array users.
+    const users = db.getUsers();
+
+    if(!users) {
+        return res.status(500).json({ message: "The users information could not be retrieved." });
+    } else {
+        return res.status(200).json(users);
+    }
 });
 
 server.get('/api/users/:id', (req, res) => {
-    //Returns the user object with the specified id.
+    const user = db.getUserById(req.params.id);
+
+    if(!user) {
+        return res.status(404).json({ message: "The user with the specified ID does not exist." });
+    } else {
+        return res.status(200).json(user);
+    }
 });
 
 server.delete('/api/users/:id', (req, res) => {
-    //Removes the user with the specified id and returns the deleted user.
+    const user = db.getUserById(req.params.id);
+
+    if(user) {
+        db.deleteUser(user.id);
+        res.status(204).end();
+    } else {
+        return res.status(404).json({ message: "The user with the specified ID does not exist." });
+    }
 });
 
 server.put('/api/users/:id', (req, res) => {
-    //Updates the user with the specified id using data from the request body. Returns the modified user
+    const user = db.getUserById(req.params.id);
+
+    if(user && req.body.name && req.body.bio) {
+       let newUser = db.updateUser(user.id, {
+            name: req.body.name,
+            bio: req.body.bio
+        });
+        res.status(200).json(newUser);
+    }
+    if(!user) {
+        return res.status(404).json({ message: "The user with the specified ID does not exist." });
+    }
+    if(!req.body.name || !req.body.bio) {
+        return res.status(400).json({ message: "Need a user name and bio" });
+    }
+    else {
+        return res.status(500).json({ errorMessage: "The user information could not be modified." });
+    }
 });
 
 server.listen(8000, () => console.log('API running on port 8000'));
